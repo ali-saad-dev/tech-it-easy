e3c16926206c242225d2e98fba1ff2ea47e614b9
Ali <asaad@snelstart.nl>
2023-12-05 13:12:07 +0100

    add NameToLongException

--- src/main/java/nl/novi/techiteasy/exceptions/NameToLongException.java
+++ src/main/java/nl/novi/techiteasy/exceptions/NameToLongException.java
@@ -1,11 +1,9 @@
 package nl.novi.techiteasy.exceptions;
 
+
 public class NameToLongException extends RuntimeException {
     public NameToLongException() {
-        super();
+        super("Record is to long");
     }
-
-    public NameToLongException(String message) {
-        super(message);
-    }
+    public NameToLongException(String message) {super(message);}
 }
